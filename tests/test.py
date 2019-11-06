f0c5271af5880d2fc9457c186f8f0e9f07b01041
phillmac <phillmac@users.noreply.github.com>
2019-11-06 11:46:24 +1100

    Change log level

--- tests/test.py
+++ tests/test.py
@@ -174,7 +174,7 @@ class SearchPeersTestCase(unittest.TestCase):
                 logging.info('Got db open event')
                 break
             else:
-                logging.info(f'Event:{event.event}')
+                logging.log(15, f'Event:{event.event}')
 
         self.kevalue_test = self.client.db('zdpuAuSAkDDRm9KTciShAcph2epSZsNmfPeLQmxw6b5mdLmq5/keyvalue_test', json={'awaitOpen': False, 'awaitLoad': False})
 
