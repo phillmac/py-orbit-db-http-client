c9279ff763459650cb072d37c7c02a2e0304eaee
phillmac <phillmac@users.noreply.github.com>
2019-11-06 17:09:07 +1100

    Wait for db load event

--- tests/test.py
+++ tests/test.py
@@ -168,23 +168,25 @@ class SearchPeersTestCase(unittest.TestCase):
             headers={'connection':'close'},      #TODO: See https://github.com/encode/httpx/issues/96
         )
 
-        events = self.client.events('open')
+        events = self.client.events('open,load')
         self.client.open_db('zdpuAuSAkDDRm9KTciShAcph2epSZsNmfPeLQmxw6b5mdLmq5/keyvalue_test', json={'awaitOpen': False, 'awaitLoad': False})
+
         for event in events:
             if event.event == 'open' and json.loads(event.data)['address'] == '/orbitdb/zdpuAuSAkDDRm9KTciShAcph2epSZsNmfPeLQmxw6b5mdLmq5/keyvalue_test':
-                logging.info('Got db open event')
+                logging.log(15,'Got db open event')
+                break
+            else:
+                logging.log(15, f'Event: {event.event} Data: {pformat(event.data)}')
+
+        for event in events:
+            if event.event == 'load' and json.loads(event.data)['address'] == '/orbitdb/zdpuAuSAkDDRm9KTciShAcph2epSZsNmfPeLQmxw6b5mdLmq5/keyvalue_test':
+                logging.log(15,'Got db load event')
                 break
             else:
                 logging.log(15, f'Event: {event.event} Data: {pformat(event.data)}')
 
         self.kevalue_test = self.client.db('zdpuAuSAkDDRm9KTciShAcph2epSZsNmfPeLQmxw6b5mdLmq5/keyvalue_test')
-        dbEvents = self.kevalue_test.events('load')
-        for event in dbEvents:
-            if event.event == 'load' and json.loads(event.data)['address'] == '/orbitdb/zdpuAuSAkDDRm9KTciShAcph2epSZsNmfPeLQmxw6b5mdLmq5/keyvalue_test':
-                logging.info('Got db load event')
-                break
-            else:
-                logging.log(15, f'Event: {event.event} Data: {pformat(event.data)}')
+
 
     def runTest(self):
         self.kevalue_test.findPeers(useCustomFindProvs=True)
