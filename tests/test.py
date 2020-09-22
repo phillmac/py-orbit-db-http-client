679950df80f47282f7e8da86770d8c718e2152d8
phillmac <phillmackintosh@gmail.com>
2020-09-22 04:05:34 +0000

    Remove SearchPeersTestCase

--- tests/test.py
+++ tests/test.py
@@ -11,35 +11,48 @@ from time import sleep
 
 from orbitdbapi.client import OrbitDbAPI
 
-base_url=os.environ.get('ORBIT_DB_HTTP_API_URL')
+base_url = os.environ.get('ORBIT_DB_HTTP_API_URL')
 timeout = int(os.environ.get('ORBIT_DB_HTTP_API_TIMEOUT', 120))
+
 
 def randString(k=5, lowercase=False, both=False):
     if both:
         return ''.join(random.choices(string.ascii_letters + string.digits, k=k))
     if lowercase:
         return ''.join(random.choices(string.ascii_lowercase + string.digits, k=k))
-    return  ''.join(random.choices(string.ascii_uppercase + string.digits, k=k))
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=k))
+
 
 class CapabilitiesTestCase(unittest.TestCase):
     def setUp(self):
         self.client = OrbitDbAPI(
             base_url=base_url,
-            headers={'connection':'close'}, #TODO: See https://github.com/encode/httpx/issues/96
+            # TODO: See https://github.com/encode/httpx/issues/96
+            headers={'connection': 'close'},
             timeout=timeout
         )
-        self.kevalue_test = self.client.db('keyvalue_cap_test', json={'create':True, 'type': 'keyvalue'})
-        self.feed_test = self.client.db('feed_cap_test', json={'create':True, 'type': 'feed'})
-        self.event_test = self.client.db('event_cap_test', json={'create':True, 'type': 'eventlog'})
-        self.docstore_test = self.client.db('docstore_cap_test', json={'create':True, 'type': 'docstore'})
-        self.counter_test = self.client.db('counter_cap_test', json={'create':True, 'type': 'counter'})
+        self.kevalue_test = self.client.db('keyvalue_cap_test', json={
+                                           'create': True, 'type': 'keyvalue'})
+        self.feed_test = self.client.db(
+            'feed_cap_test', json={'create': True, 'type': 'feed'})
+        self.event_test = self.client.db(
+            'event_cap_test', json={'create': True, 'type': 'eventlog'})
+        self.docstore_test = self.client.db('docstore_cap_test', json={
+                                            'create': True, 'type': 'docstore'})
+        self.counter_test = self.client.db('counter_cap_test', json={
+                                           'create': True, 'type': 'counter'})
 
     def runTest(self):
-        self.assertEqual(set(['get', 'put', 'remove']), set(self.kevalue_test.capabilities))
-        self.assertEqual(set(['add', 'get', 'iterator', 'remove']), set(self.feed_test.capabilities))
-        self.assertEqual(set(['add', 'get', 'iterator']), set(self.event_test.capabilities))
-        self.assertEqual(set(['get', 'put', 'putAll', 'query', 'remove']), set(self.docstore_test.capabilities))
-        self.assertEqual(set(['inc', 'value']), set(self.counter_test.capabilities))
+        self.assertEqual(set(['get', 'put', 'remove']),
+                         set(self.kevalue_test.capabilities))
+        self.assertEqual(set(['add', 'get', 'iterator', 'remove']), set(
+            self.feed_test.capabilities))
+        self.assertEqual(set(['add', 'get', 'iterator']),
+                         set(self.event_test.capabilities))
+        self.assertEqual(set(['get', 'put', 'putAll', 'query', 'remove']), set(
+            self.docstore_test.capabilities))
+        self.assertEqual(set(['inc', 'value']), set(
+            self.counter_test.capabilities))
 
     def tearDown(self):
         self.kevalue_test.unload()
@@ -49,20 +62,23 @@ class CapabilitiesTestCase(unittest.TestCase):
         self.counter_test.unload()
         self.client.close()
 
+
 class CounterIncrementTestCase(unittest.TestCase):
     def setUp(self):
         self.client = OrbitDbAPI(
             base_url=base_url,
-            headers={'connection':'close'},      #TODO: See https://github.com/encode/httpx/issues/96
+            # TODO: See https://github.com/encode/httpx/issues/96
+            headers={'connection': 'close'},
             timeout=timeout
         )
-        self.counter_test = self.client.db('counter_store_test', json={'create':True, 'type': 'counter'})
+        self.counter_test = self.client.db('counter_store_test', json={
+                                           'create': True, 'type': 'counter'})
 
     def runTest(self):
         localVal = self.counter_test.value()
         self.assertEqual(localVal, self.counter_test.value())
-        for _c in range(1,100):
-            incVal = random.randrange(1,100)
+        for _c in range(1, 100):
+            incVal = random.randrange(1, 100)
             localVal += incVal
             self.counter_test.inc(incVal)
             self.assertEqual(localVal, self.counter_test.value())
@@ -76,32 +92,33 @@ class KVStoreTestCase(unittest.TestCase):
         self.client = OrbitDbAPI(
             base_url=base_url,
             use_db_cache=False,
-            headers={'connection':'close'}, #TODO: See https://github.com/encode/httpx/issues/96
+            # TODO: See https://github.com/encode/httpx/issues/96
+            headers={'connection': 'close'},
             timeout=timeout
         )
-        self.kevalue_test = self.client.db('keyvalue_store_test', json={'create':True, 'type': 'keyvalue'})
+        self.kevalue_test = self.client.db('keyvalue_store_test', json={
+                                           'create': True, 'type': 'keyvalue'})
 
     def runTest(self):
         self.assertFalse(self.kevalue_test.cached)
         localKV = {}
-        for _c in range(1,100):
+        for _c in range(1, 100):
             k = randString()
             v = randString(k=100, both=True)
             localKV[k] = v
-            self.kevalue_test.put({'key':k, 'value':v})
+            self.kevalue_test.put({'key': k, 'value': v})
             self.assertEqual(localKV.get(k), self.kevalue_test.get(k))
 
         self.assertDictContainsSubset(localKV, self.kevalue_test.all())
 
         DeletedKeys = []
-        for _c in range(1,75):
-            delk = random.choice([k for k in localKV.keys() if not k in DeletedKeys])
+        for _c in range(1, 75):
+            delk = random.choice(
+                [k for k in localKV.keys() if not k in DeletedKeys])
             DeletedKeys.append(delk)
             self.kevalue_test.remove(delk)
         remoteKeys = self.kevalue_test.all().keys()
         self.assertTrue(all(k not in remoteKeys for k in DeletedKeys))
-
-
 
     def tearDown(self):
         self.kevalue_test.unload()
@@ -113,18 +130,20 @@ class DocStoreTestCase(unittest.TestCase):
         self.client = OrbitDbAPI(
             base_url=base_url,
             use_db_cache=False,
-            headers={'connection':'close'}, #TODO: See https://github.com/encode/httpx/issues/96
+            # TODO: See https://github.com/encode/httpx/issues/96
+            headers={'connection': 'close'},
             timeout=timeout
         )
-        self.docstore_test = self.client.db('docstore_store_test', json={'create':True, 'type': 'docstore'})
+        self.docstore_test = self.client.db('docstore_store_test', json={
+                                            'create': True, 'type': 'docstore'})
 
     def runTest(self):
         self.assertFalse(self.docstore_test.cached)
         localDocs = []
-        for _c in range(1,100):
+        for _c in range(1, 100):
             k = randString()
             v = randString(k=100, both=True)
-            item = {'_id':k, 'value':v}
+            item = {'_id': k, 'value': v}
             localDocs.append(item)
             self.docstore_test.put(item)
             self.assertDictContainsSubset(item, self.docstore_test.get(k)[0])
@@ -133,72 +152,21 @@ class DocStoreTestCase(unittest.TestCase):
         self.assertTrue(all(item in remoteDocs for item in localDocs))
 
         DeletedDocs = []
-        for _c in range(1,75):
-            item = random.choice([d for d in localDocs if not d in DeletedDocs])
+        for _c in range(1, 75):
+            item = random.choice(
+                [d for d in localDocs if not d in DeletedDocs])
             DeletedDocs.append(item)
             self.docstore_test.remove(item['_id'])
         remoteDocs = self.docstore_test.all()
         self.assertTrue(all(d not in remoteDocs for d in DeletedDocs))
-
 
     def tearDown(self):
         self.docstore_test.unload()
         self.client.close()
 
 
-class SearchPeersTestCase(unittest.TestCase):
-    def setUp(self):
-        self.client = OrbitDbAPI(
-            base_url=base_url,
-            headers={'connection':'close'},      #TODO: See https://github.com/encode/httpx/issues/96
-            timeout=timeout
-        )
-
-        events = self.client.events('open,ready,load')
-
-        for event in events:
-            if event.event == 'registered':
-                logging.log(15, f'Got registered event: {pformat(event.json)}')
-                break
-            else:
-                logging.log(15, f'Event: {event.event} Data: {pformat(event.json)}')
-
-        dbInfo = self.client.open_db('zdpuAuSAkDDRm9KTciShAcph2epSZsNmfPeLQmxw6b5mdLmq5/keyvalue_test', json={'awaitOpen': False})
-        if (not 'ready' in dbInfo) or (not dbInfo['ready']):
-            logging.info('Waiting for db to be ready...')
-
-            for event in events:
-                if event.event == 'ready' and event.json['address'] == '/orbitdb/zdpuAuSAkDDRm9KTciShAcph2epSZsNmfPeLQmxw6b5mdLmq5/keyvalue_test':
-                    logging.log(15,'Got db ready event')
-                    break
-                else:
-                    logging.log(15, f'Event: {event.event} Data: {pformat(event.json)}')
-
-        self.kevalue_test = self.client.db('zdpuAuSAkDDRm9KTciShAcph2epSZsNmfPeLQmxw6b5mdLmq5/keyvalue_test')
-        self.assertTrue(self.kevalue_test.info()['ready'])
-
-
-    def runTest(self):
-        self.kevalue_test.find_peers(useCustomFindProvs=True)
-        dbPeers = []
-        count = 0
-        while len(dbPeers) < 1:
-            self.assertTrue('/orbitdb/zdpuAuSAkDDRm9KTciShAcph2epSZsNmfPeLQmxw6b5mdLmq5/keyvalue_test' in [s['searchID'] for s in self.client.searches()])
-            sleep(5)
-            dbPeers = self.kevalue_test.get_peers()
-            if count > 60: break
-            count +=1
-        self.assertGreater(len(dbPeers), 0)
-
-
-    def tearDown(self):
-        self.kevalue_test.unload()
-        self.client.close()
-
-
-
 if __name__ == '__main__':
-    loglvl = int(os.environ.get('LOG_LEVEL',15))
+    loglvl = int(os.environ.get('LOG_LEVEL', 15))
     print(f'Log level: {loglvl}')
     print(f'Timeout: {timeout} seconds')
     logfmt = '%(asctime)s - %(levelname)s - %(message)s'
